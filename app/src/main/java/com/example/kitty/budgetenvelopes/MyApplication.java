0c79e5e6064a6199b4519bf962df02196525e03d
codingkitty81 <codingkitty81@gmail.com>
2018-02-16 14:39:06 -0800

    Starting Realm Setup

---
+++ app/src/main/java/com/example/kitty/budgetenvelopes/MyApplication.java
@@ -0,0 +1,17 @@
+package com.example.kitty.budgetenvelopes;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+/**
+ * Created by Kitty on 2/16/2018.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
---
+++ app/src/main/java/com/example/kitty/budgetenvelopes/model/Transaction.java
@@ -0,0 +1,11 @@
+package com.example.kitty.budgetenvelopes.model;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by Kitty on 2/16/2018.
+ */
+
+public class Transaction extends RealmObject {
+
+}
